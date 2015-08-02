814a9266f5c668a0cc7a28c4f542c321e666721d
Yifeng Yang <yang-yifeng@hotmail.com>
2015-08-02 15:51:35 +0700

    login method completed

--- pair-ionic/www/js/controllers.js
+++ pair-ionic/www/js/controllers.js
@@ -12,10 +12,23 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('LoginCtrl',function ($scope) {
+.controller('LoginCtrl',function ($scope,PopUp,$state) {
 
   $scope.login = function (form,user) {
 
+      if(!form.$valid){
+        PopUp.showPopUp({
+           title: 'Invalid Email/Password',
+           template: 'Please check and try again',
+           okType: 'button-large button-assertive'
+        },null);
+      }
+      else
+      {
+        //form valid attemps login
+        console.log(user);
+        $state.go('tab.dash');
+      }
   };
 
 
--- pair-ionic/www/js/services.js
+++ pair-ionic/www/js/services.js
@@ -1,1 +1,31 @@
 angular.module('starter.services', [])
+.factory('PopUp',function ($ionicPopup,$timeout) {
+
+  return{
+      /*
+      popUpContent
+        tilte:
+        template:
+        timeout: in millisecs
+      */
+    showPopUp: function (popUpContent,callback) {
+      var alertPopUp = $ionicPopup.alert({
+        title: popUpContent.title,
+        template: popUpContent.template,
+        okType: popUpContent.okType
+      });
+      alertPopUp.then(function (res) {
+        if(callback){
+          callback();
+        }
+      });
+      if(popUpContent.timeout){
+        $timeout(function () {
+          alertPopUp.close();
+        },popUpContent.timeout)
+      }
+    }
+  }
+
+})
+
