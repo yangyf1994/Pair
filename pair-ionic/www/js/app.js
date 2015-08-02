6f0dc0a8af614b5536ed757d897e085d071b0edd
Yifeng Yang <yang-yifeng@hotmail.com>
2015-08-02 15:11:29 +0700

    first commit

---
+++ pair-ionic/www/js/app.js
@@ -0,0 +1,64 @@
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleLightContent();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+   .state('login',{
+    url:'/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/login');
+
+    console.log('Config Initiated');
+});
---
+++ pair-ionic/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+
+})
+
+.controller('ChatsCtrl', function($scope) {
+
+})
+
+.controller('AccountCtrl', function($scope) {
+
+})
+
+.controller('LoginCtrl',function ($scope) {
+  console.log('LoginCtrl');
+  $scope.login = function (form,user) {
+
+  };
+
+
+});
